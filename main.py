8c1b508ff3d3dd120956c2c10f17a4904043b87a
acoyp99 <acoyp1999@gmail.com>
2025-06-17 22:59:08 -0500

    artifactory: Workflows and codespace devcontainer configuration

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import redis
+
 print('Hello from GitHub Actions')
+
+def connect_and_set_key():
+    try:
+        r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+        r.set('mykey', 'Hello, Redis!')
+        value = r.get('mykey')
+        print(f"Value for 'mykey': {value}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    connect_and_set_key()
